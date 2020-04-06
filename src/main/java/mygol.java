5b84d6a4894b5b1ca4e1641988200269d8c3d939
zyy17111847 <1585276573@qq.com>
2020-04-07 00:37:58 +0800

    第一次测试

---
+++ src/main/java/mygol.java
@@ -0,0 +1,10 @@
+public class mygol {
+
+    public static void main(String[] args) {
+        System.out.println("dsdsdsd");
+        System.out.println("dddd11sdsdsd");
+        System.out.println("dsd32dddsdsd");
+        System.out.println("dsds2333dsd");
+
+    }
+}
